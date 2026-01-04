8e76a6cc78abbd04df6ee5c6e8ff5dd1d97d71b4
kiakarbon <kiaaroya@gmail.com>
2026-01-04 09:57:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,3 @@
-# app.py
-import streamlit as st
-import pandas as pd
-import plotly.express as px
-from datetime import datetime
-
 # st.set_page_config(
      page_title="Ex-stream-ly Cool App",
      page_icon="🦈",
