32c061ba21d308663eba573e443bfd0c20ffb28a
kiakarbon <kiaaroya@gmail.com>
2026-01-04 09:34:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
+# app.py
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+from datetime import datetime
+
+# Konfigurasi halaman
+st.set_page_config(page_title="Personal Finance Dashboard", layout="wide")
 
